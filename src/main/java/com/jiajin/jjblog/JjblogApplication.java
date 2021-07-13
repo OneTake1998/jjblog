d4f99086d31ab93ab470a8d374fd3da44be99730
jiajin <670626858@qq.com>
2021-07-13 11:30:47 +0800

    initialize project

---
+++ src/main/java/com/jiajin/jjblog/JjblogApplication.java
@@ -0,0 +1,13 @@
+package com.jiajin.jjblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jiajin/jjblog/JjblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiajin.jjblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
